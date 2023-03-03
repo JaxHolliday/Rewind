9ae6d5dc4b09eec631dbdc94f1271c462607a51a
Jackson Holliday <jaxholliday@gmail.com>
2023-03-03 10:22:27 -0500

    ui setup and psudo code input

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//call Hello Word 
-function HelloWorld(){
-    alert("Hello World");
+//get string value from page 
+//controller function
+function getValues(){
+    //add any invis class to page// makes it available 
+   document.getElementById("alert").classList.add("invisible");
+
+   let userString = document.getElementById("userString").value;
+
+   let revString = reverseString(userString);
+
+   displayString(revString);
+}
+
+//reverse the string
+//logic function
+function reverseString(userString){
+
+}
+
+//display the results
+//view function
+function displayString(){
+
 }
